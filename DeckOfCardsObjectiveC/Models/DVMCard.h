30d925c1660366fc78099f37a677f43d52714491
Ian Becker <ian-becker@lambdastudents.com>
2020-08-11 13:19:31 -0700

    Model complete

--- DeckOfCardsObjectiveC/Models/DVMCard.h
+++ DeckOfCardsObjectiveC/Models/DVMCard.h
@@ -10,31 +10,12 @@
 
 @interface DVMCard : NSObject
 
-/// When defining a property in our header file, we start with @ property, we then define our property attributes, followed by the type and pointer in memory and finally the property name. Don't forget your semicolon!
 @property (nonatomic, copy, readonly) NSString *suit;
 
-/// When defining a property in our header file, we start with @ property, we then define our property attributes, followed by the type and pointer in memory and finally the property name. Don't forget your semicolon!
 @property (nonatomic, copy, readonly) NSString *imageString;
 
-/**
-@brief Initializes a new DVMCard with the provided suit and image.
-
- @param suit The suit name of the card (Diamonds, Hearts, Spades or Clubs)
- @param imageString The image of the card face
-
-@return A DVMCard with proper image and suit defined
-
-*/
 - (instancetype)initWithSuit:(NSString *)suit imageString:(NSString *)imageString;
 
-/**
-Initializes a new DVMCard from a provided dictionary.
-
- @param dictionary A dictionary to be decoded into a DVMCard
-
-@return A DVMCard with proper image and suit defined by calling our memberwise initializer
-
-*/
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
 @end
--- DeckOfCardsObjectiveC/Models/DVMCard.m
+++ DeckOfCardsObjectiveC/Models/DVMCard.m
@@ -8,34 +8,26 @@
 
 #import "DVMCard.h"
 
-// String values to avoid magic strings
-static NSString * const kSuitKey = @"suit";
-static NSString * const kImageKey = @"image";
+static NSString * const suitKey = @"suit";
+static NSString * const imageKey = @"image";
 
 @implementation DVMCard
 
 - (instancetype)initWithSuit:(NSString *)suit imageString:(NSString *)imageString;
 {
-    // We first must initialize our superclass since our class inherits from NSObject
     if (self = [super init])
     {
-        //The below lines are the same as 'self.suit = suit' & 'self.imageString = imageString' in swift
         _suit = suit;
         _imageString = imageString;
     }
-    
-    // once with have initialized the card, we can return it.
     return self;
 }
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary
 {
-    // The below lines of code pull the value out of the passed in dictionary at the specified key.
-    NSString *suit = dictionary[kSuitKey];
-    NSString *imageString = dictionary[kImageKey];
+    NSString *suit = dictionary[suitKey];
+    NSString *imageString = dictionary[imageKey];
     
-    // Once we have the values from our dictionary, we can initialize a card object with those values
     return [self initWithSuit:suit imageString:imageString];
 }
-                                
 @end
